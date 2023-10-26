c0337994005bb459f5a0b54451942bdf981ac942
Patrick Orjieh <orjiehpatrick@gmail.com>
2023-10-27 00:52:15 +0100

    add styles navbar and jumbotron

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { BeatLoader } from 'react-spinners';
+import logo from './logo.jpg';
+import './App.css';
 
 function App() {
   const [country, setCountry] = useState('');
@@ -40,17 +42,45 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <h1>Country Information</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          value={country}
-          onChange={handleInputChange}
-          placeholder="Enter country name"
-        />
-        <button type="submit">Search</button>
-      </form>
+    <div className="App" class="container-fluid">
+      <nav class="navbar">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            <img src={logo} alt="" width="60" height="60" class="d-inline-block align-text-top" /> &nbsp;
+            Countries Coding Challenge - Patrick Orjieh
+          </a>
+        </div>
+      </nav>
+
+      <div className="jumbotron mb-3">
+        <h2 className="display-6">Welcome to Countries Coding Challenge</h2>
+        <p className="lead">Discover information about countries worldwide.</p>
+        <hr className="my-4" />
+        <div class="text-center">
+          <p>Select a starting letter to view countries:</p>
+          <div className="alphabet-list">
+            {['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'].map(letter => (
+              <button className="btn btn-outline-primary mr-2" key={letter}>
+                {letter}
+              </button>
+            ))}
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            value={country}
+            onChange={handleInputChange}
+            placeholder="Enter country name"
+          />
+          <button type="submit">Search</button>
+        </form>
+      </div>
+
+
 
       {loading && <BeatLoader color={"#123abc"} />}
       {error && <p>{error}</p>}
