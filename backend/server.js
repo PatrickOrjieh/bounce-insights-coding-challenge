a8c517a3e20e2c2723162c31d021b6efc5a2c673
Patrick Orjieh <orjiehpatrick@gmail.com>
2023-10-26 22:30:46 +0100

    Add enhanced error handling

--- backend/server.js
+++ backend/server.js
@@ -9,20 +9,32 @@ app.use(cors());
 
 app.get('/country/:name', (req, res) => {
     const countryName = req.params.name;
-    
+
     axios.get(`https://restcountries.com/v3.1/name/${countryName}`)
-      .then(response => {
-        res.json(response.data);
-      })
-      .catch(error => {
-        res.status(500).json({ error: "Error fetching country data" });
-      });
-  });
+        .then(response => {
+            if (response.data && response.data.length > 0) {
+                res.json(response.data);
+            } else {
+                res.status(404).json({ error: "Country not found" });
+            }
+        })
+        .catch(error => {
+            res.status(500).json({ error: "Error fetching country data" });
+        });
+});
 
 app.get('/test', (req, res) => {
-  res.send('Backend connection successful!');
+    res.send('Backend connection successful!');
+});
+
+app.use((req, res, next) => {
+    res.status(404).json({ error: "Endpoint not found" });
+});
+
+app.use((error, req, res, next) => {
+    res.status(500).json({ error: error.message });
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
