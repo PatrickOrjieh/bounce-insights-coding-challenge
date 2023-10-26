1f4282b963b2b9928a147be68966298b78b4b68a
Patrick Orjieh <orjiehpatrick@gmail.com>
2023-10-26 22:27:44 +0100

    SetUp Basic Express server and routes

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 const PORT = 5000;
 
 app.use(cors());
+
+app.get('/country/:name', (req, res) => {
+    const countryName = req.params.name;
+    
+    axios.get(`https://restcountries.com/v3.1/name/${countryName}`)
+      .then(response => {
+        res.json(response.data);
+      })
+      .catch(error => {
+        res.status(500).json({ error: "Error fetching country data" });
+      });
+  });
 
 app.get('/test', (req, res) => {
   res.send('Backend connection successful!');
